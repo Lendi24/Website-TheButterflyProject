08a9dc529ade1550d3e2dc5f296dd631a280d420
lendi24 <lendic24@hotmail.com>
2021-12-03 14:56:22 +0100

    Added a broken divider. (better than no divider)

--- js/main.js
+++ js/main.js
@@ -1,28 +1,43 @@
 window.onload = function() {
-    window.onscroll = function() {stickyNavbar()};
-
     var nav = document.getElementsByClassName("nav-button-container")[0];
     var navOffset = nav.offsetTop;
-    
-    function stickyNavbar() {
-      if (window.pageYOffset >= navOffset) {
-        nav.classList.add("nav-fixed-pos")
-      } else {
-        nav.classList.remove("nav-fixed-pos");
-      }
-    }
+    var dividers = document.getElementsByClassName("divider-rot-left");
+
+    window.onscroll = function() {
+        stickyNavbar(nav, navOffset);
+    };
+
+    window.onresize = function() {
+        RotationalDivider(dividers);
+    };
 };
 
-function SwitchPage(pageIndex) {
-  var pages = document.getElementsByClassName("page");
+function RotationalDivider(dividers) {
+    var newRotate = Math.atan(dividers[0].clientHeight / dividers[0].clientWidth) * 180 / Math.PI;
 
-  for (let i = 0; i < pages.length; i++) {
-      pages[i].classList.remove("hidden");
-  }
-
-  for (let i = 0; i < pages.length; i++) {
-    if (i != pageIndex) {
-      pages[i].classList.add("hidden");
+    for (let i = 0; i < dividers.length; i++) {
+        dividers[i].style = "rotate: " + newRotate + "deg;";
     }
-  }
+}
+
+function stickyNavbar(nav, navOffset) {
+    if (window.pageYOffset >= navOffset) {
+        nav.classList.add("nav-fixed-pos")
+    } else {
+        nav.classList.remove("nav-fixed-pos");
+    }
+}
+
+function SwitchPage(pageIndex) {
+    var pages = document.getElementsByClassName("page");
+
+    for (let i = 0; i < pages.length; i++) {
+        pages[i].classList.remove("hidden");
+    }
+
+    for (let i = 0; i < pages.length; i++) {
+        if (i != pageIndex) {
+            pages[i].classList.add("hidden");
+        }
+    }
 }
